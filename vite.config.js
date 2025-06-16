3cd77431cd818948ba5cb3ac70de248e8e8bc6e6
apaqa <160603128+apaqa@users.noreply.github.com>
2025-06-16 15:19:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-multifunction-portfolio/',
+  base: '/Javascript/',
   plugins: [react()]
 });
